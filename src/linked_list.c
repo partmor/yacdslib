c39b618a955ce2591e444eee3d32ee17c469af0e
partmor <part.morales@gmail.com>
2020-08-23 17:01:13 +0200

    implement linked list push

--- src/linked_list.c
+++ src/linked_list.c
@@ -2,9 +2,17 @@
 #include "linked_list.h"
 
 
-node_t* create_head(int val){
+node_t* create_node(int val){
     node_t* head = malloc(sizeof(node_t));
     head->val = val;
     head->next = NULL;
     return head;
+}
+
+void push(node_t* head, int val){
+    node_t* node = head;
+    while(node->next){
+        node = node->next;
+    }
+    node->next = create_node(val);
 }
--- src/linked_list.h
+++ src/linked_list.h
@@ -5,7 +5,8 @@ typedef struct node {
     struct node* next;
 } node_t;
 
-node_t* create_head(int val);
+node_t* create_node(int val);
+void push(node_t* head, int val);
 
 #define yacdslib_LINKED_LIST_H
 #endif //yacdslib_LINKED_LIST_H
