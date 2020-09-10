6282182866d5876e4c8e95faaf0936f93eef067e
partmor <part.morales@gmail.com>
2020-09-10 22:34:16 +0200

    refactor linked list api

--- tests/test_linked_list.c
+++ tests/test_linked_list.c
@@ -2,97 +2,110 @@
 #include "test_linked_list.h"
 #include "../src/linked_list.h"
 
-START_TEST(test_create_node){
-    node_t* head = create_node(1);
-    ck_assert_ptr_nonnull(head);
-    ck_assert_int_eq(head->val, 1);
-    ck_assert_ptr_null(head->next);
+
+START_TEST(test_list_init){
+    list_t list;
+    list_init(&list);
+
+    ck_assert_ptr_null(list.head);
+    ck_assert_ptr_null(list.tail_sentinel);
+    ck_assert_int_eq(list.number_of_elements, 0);
 }
 END_TEST
 
-START_TEST(test_push_back){
-    /* Create a 3-node list: 1 -> 2 -> 3 */
-    node_t* head = create_node(1);
-    push_back(head, 2);
-    push_back(head, 3);
+START_TEST(test_list_append_to_empty_list){
+    list_t list;
+    list_init(&list);
+    list_append(&list, 10);
 
-    /* Traverse the list and finally check the last node point's next
-     * points to NULL */
-    node_t* node = head;
+    ck_assert_ptr_nonnull(list.head);
+    ck_assert_ptr_null(list.tail_sentinel);
+    ck_assert_int_eq(list.head->val, 10);
+    ck_assert_int_eq(list.number_of_elements, 1);
+    ck_assert_ptr_eq(list.head->next, list.tail_sentinel);
+}
+END_TEST
+
+START_TEST(test_list_append_to_populated_list){
+    list_t list;
+    list_init(&list);
+    list_append(&list, 10);
+    list_append(&list, 20);
+
+    ck_assert_ptr_nonnull(list.head);
+    ck_assert_ptr_null(list.tail_sentinel);
+    ck_assert_int_eq(list.number_of_elements, 2);
+    node_t* node = list.head;
+    for(size_t i = 1; i <= list.number_of_elements; ++i){
+        ck_assert_int_eq(node->val, i * 10);
+        node = node->next;
+    }
+    ck_assert_ptr_eq(node, list.tail_sentinel);
+}
+END_TEST
+
+START_TEST(test_list_get_element_at_location){
+    list_t list;
+    list_init(&list);
+    list_append(&list, 10);
+    list_append(&list, 20);
+    list_append(&list, 30);
+
+    ck_assert_int_eq(list_get_element(list, 0), 10);
+    ck_assert_int_eq(list_get_element(list, 1), 20);
+    ck_assert_int_eq(list_get_element(list, 2), 30);
+}
+END_TEST
+
+START_TEST(test_create_list_from_array){
+    int arr[3] = {10, 20, 30};
+    list_t list;
+    list_init_from_array(&list, arr, 3);
+
+    node_t* node = list.head;
     for(size_t i = 1; i <= 3; ++i){
-        ck_assert_int_eq(node->val, i);
+        ck_assert_int_eq(node->val, i * 10);
         node = node->next;
     }
     ck_assert_ptr_null(node);
+    ck_assert_ptr_eq(node, list.tail_sentinel);
 }
 END_TEST
 
-START_TEST(test_create_from_array){
-    /* Create a 3-node list: 1 -> 2 -> 3 */
-    int arr[3] = {1, 2, 3};
-    node_t* head = create_list_from_array(arr, 3);
+START_TEST(test_create_list_prepend){
+    int arr[3] = {10, 20, 30};
+    list_t list;
+    list_init_from_array(&list, arr, 3);
 
-    /* Traverse the list and finally check the last node point's next
-     * points to NULL */
-    node_t* node = head;
-    for(size_t i = 1; i <= 3; ++i){
-        ck_assert_int_eq(node->val, i);
-        node = node->next;
-    }
-    ck_assert_ptr_null(node);
+    list_prepend(&list, 100);
+
+    ck_assert_int_eq(list.head->val, 100);
+    ck_assert_int_eq(list.head->next->val, 10);
 }
 END_TEST
 
-START_TEST(test_push_front){
-    /* Create a 3-node list: 1 -> 2 -> 3 */
-    int arr[3] = {1, 2, 3};
-    node_t* head = create_list_from_array(arr, 3);
+START_TEST(test_free_list_memory){
+    int arr[3] = {10, 20, 30};
+    list_t list;
+    list_init_from_array(&list, arr, 3);
 
-    /* Push a node to the front and retrieve new head
-     * the sequence should now be 0 -> 1 -> 2 -> 3 */
-    head = push_front(head, 0);
+    list_free_memory(&list);
 
-    /* Traverse the list and finally check the last node point's next
-     * points to NULL */
-    node_t* node = head;
-    for(size_t i = 0; i <= 3; ++i){
-        ck_assert_int_eq(node->val, i);
-        node = node->next;
-    }
-    ck_assert_ptr_null(node);
+    ck_assert_ptr_null(list.head);
+    ck_assert_int_eq(list.number_of_elements, 0);
 }
 END_TEST
 
-START_TEST(test_size){
-    /* Create a 3-node list: 1 -> 2 -> 3 */
-    int arr[3] = {1, 2, 3};
-    node_t* head = create_list_from_array(arr, 3);
-    ck_assert_int_eq(size(head), 3);
+START_TEST(test_list_get_size){
+    int arr[4] = {10, 20, 30, 40};
+    list_t list;
+    list_init_from_array(&list, arr, 4);
+
+    ck_assert_int_eq(list_get_size(list), 4);
+    ck_assert_int_eq(list_get_size(list), list.number_of_elements);
 }
 END_TEST
 
-START_TEST(test_get_pos){
-    int arr[5] = {0, 1, 2, 3, 4};
-    node_t* head = create_list_from_array(arr, 5);
-    node_t* node = get_node(head, 2);
-    ck_assert_int_eq(node->val, 2);
-}
-END_TEST
-
-START_TEST(test_peek_last){
-    int arr[5] = {0, 1, 2, 3, 4};
-    node_t* head = create_list_from_array(arr, 5);
-    node_t* node = peek_last(head);
-    ck_assert_int_eq(node->val, 4);
-}
-END_TEST
-
-START_TEST(test_free_list_no_errors){
-    int arr[5] = {0, 1, 2, 3, 4};
-    node_t* head = create_list_from_array(arr, 5);
-    free_linked_list(head);
-}
-END_TEST
 
 Suite* make_linked_list_suite(void)
 {
@@ -104,14 +117,14 @@ Suite* make_linked_list_suite(void)
     /* Core test case */
     tc_core = tcase_create("Core");
 
-    tcase_add_test(tc_core, test_create_node);
-    tcase_add_test(tc_core, test_push_back);
-    tcase_add_test(tc_core, test_create_from_array);
-    tcase_add_test(tc_core, test_push_front);
-    tcase_add_test(tc_core, test_size);
-    tcase_add_test(tc_core, test_get_pos);
-    tcase_add_test(tc_core, test_peek_last);
-    tcase_add_test(tc_core, test_free_list_no_errors);
+    tcase_add_test(tc_core, test_list_init);
+    tcase_add_test(tc_core, test_list_append_to_empty_list);
+    tcase_add_test(tc_core, test_list_append_to_populated_list);
+    tcase_add_test(tc_core, test_list_get_element_at_location);
+    tcase_add_test(tc_core, test_create_list_from_array);
+    tcase_add_test(tc_core, test_create_list_prepend);
+    tcase_add_test(tc_core, test_free_list_memory);
+    tcase_add_test(tc_core, test_list_get_size);
 
     suite_add_tcase(s, tc_core);
 
