377875822d2b28c7a34897670a1bb6a0b446bc67
partmor <part.morales@gmail.com>
2020-09-10 22:34:16 +0200

    refactor linked list api

--- src/linked_list.c
+++ src/linked_list.c
@@ -1,69 +1,84 @@
 #include <stdlib.h>
 #include "linked_list.h"
 
-
-node_t* create_node(int val){
-    node_t* head = malloc(sizeof(node_t));
-    head->val = val;
-    head->next = NULL;
-    return head;
+void list_node_init(node_t** node, int val, node_t* next){
+    (*node) = malloc(sizeof(node_t));
+    (*node)->val = val;
+    (*node)->next = next;
 }
 
-node_t* peek_last(node_t* head){
-    node_t* node = head;
-    while (node->next){
+void list_init(list_t* list_ptr){
+    list_ptr->head = NULL;
+    list_ptr->tail_sentinel = NULL;
+    list_ptr->number_of_elements = 0;
+}
+
+void list_append_to_empty_list(list_t *list_ptr, int val) {
+    list_node_init(&list_ptr->head, val, list_ptr->tail_sentinel);
+    list_ptr->number_of_elements++;
+}
+
+void list_append_to_populated_list(list_t *list_ptr, int val) {
+    // navigate to last node:
+    node_t* node = list_ptr->head;
+    while(node->next != list_ptr->tail_sentinel){
         node = node->next;
     }
-    return node;
+    // create the link:
+    node_t* new_node;
+    list_node_init(&new_node, val, list_ptr->tail_sentinel);
+    node->next = new_node;
+    list_ptr->number_of_elements++;
 }
 
-void push_back(node_t* head, int val){
-    node_t* node = peek_last(head);
-    node->next = create_node(val);
-}
-
-node_t* push_front(node_t* head, int val){
-    node_t* new_head = create_node(val);
-    new_head->next = head;
-    return new_head;
-}
-
-node_t* create_list_from_array(int arr[], size_t arr_size){
-    node_t* head = NULL;
-    if(arr_size){
-        head = create_node(arr[0]);
-        for(size_t i = 1; i < arr_size; ++i){
-            push_back(head, arr[i]);
-        }
+void list_append(list_t* list_ptr, int val){
+    if(!list_ptr->head){
+        list_append_to_empty_list(list_ptr, val);
+    } else{
+        list_append_to_populated_list(list_ptr, val);
     }
-    return head;
 }
 
-int size(node_t* head){
-    int count = 0;
-    node_t* node = head;
-    while (node){
+int list_get_element(list_t list, size_t loc){
+    node_t* node = list.head;
+    for(size_t count = 1; count <= loc; ++count){
+        node = node->next;
+    }
+    return node->val;
+}
+
+void list_init_from_array(list_t* list_ptr, int *arr, size_t arr_size){
+    list_init(list_ptr);
+    for(size_t ix = 0; ix < arr_size; ++ix){
+        list_append(list_ptr, arr[ix]);
+    }
+}
+
+void list_prepend(list_t* list_ptr, int val){
+    node_t* new_node;
+    list_node_init(&new_node, val, list_ptr->head);
+    list_ptr->head = new_node;
+}
+
+void list_free_memory(list_t* list_ptr){
+    node_t* node = list_ptr->head;
+    // free all nodes of the list
+    while(node != list_ptr->tail_sentinel){
+        node_t* node_to_delete = node;
+        node = node->next;
+        free(node_to_delete);
+    }
+    // finally, zero-initialize the list
+    list_init(list_ptr);
+}
+
+size_t list_get_size(list_t list){
+    // should be equal to list.number_of_elements
+    size_t count = 0;
+    node_t* node = list.head;
+    while(node != list.tail_sentinel){
         node = node->next;
         ++count;
     }
     return count;
-}
-
-node_t* get_node(node_t* head, size_t pos){
-    node_t* node = head;
-    for (size_t i = 0; i < pos; ++i) {
-        node = node->next;
-    }
-    return node;
-}
-
-void free_linked_list(node_t* head){
-    node_t* node = head;
-    node_t* to_free;
-    while(node){
-        to_free = node;
-        node = node->next;
-        free(to_free);
-        to_free = NULL; // just for peace of mind
-    }
 }
--- src/linked_list.h
+++ src/linked_list.h
@@ -5,14 +5,20 @@ typedef struct node {
     struct node* next;
 } node_t;
 
-node_t* create_node(int val);
-void push_back(node_t* head, int val);
-node_t* push_front(node_t* head, int val);
-node_t* create_list_from_array(int arr[], size_t arr_size);
-int size(node_t* head);
-node_t* get_node(node_t* head, size_t pos);
-node_t* peek_last(node_t* head);
-void free_linked_list(node_t* head);
+typedef struct list {
+    node_t* head;
+    node_t* tail_sentinel;
+
+    int number_of_elements;
+} list_t;
+
+void list_init(list_t* list_ptr);
+void list_append(list_t* list_ptr, int val);
+void list_init_from_array(list_t* list_ptr, int *arr, size_t arr_size);
+void list_prepend(list_t* list_ptr, int val);
+int list_get_element(list_t list, size_t loc);
+void list_free_memory(list_t* list_ptr);
+size_t list_get_size(list_t list);
 
 #define YACDSLIB_LINKED_LIST_H
 #endif //YACDSLIB_LINKED_LIST_H
