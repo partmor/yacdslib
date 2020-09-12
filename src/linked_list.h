aa2d417252044963a0415398f43d737ec73240ce
Pedro Morales Maries <part.morales@gmail.com>
2020-09-12 18:14:05 +0200

    Generate docs with Doxygen (#6)
    - Add placeholder for Doxygen docstrings in code.
    - Create Doxigen config and set README.md as landing/index page.
    - Add GH Action job to automatically deploy docs to gh-pages on push to master branch.

--- src/linked_list.h
+++ src/linked_list.h
@@ -1,3 +1,9 @@
+/**
+ * @file linked_list.h
+ * @author Pedro Arturo Morales Maries
+ * @brief A simple implementation of a singly linked list of integers.
+ */
+
 #ifndef YACDSLIB_LINKED_LIST_H
 
 typedef struct node {
@@ -5,14 +11,42 @@ typedef struct node {
     struct node* next;
 } node_t;
 
-typedef struct list {
+/**
+ * @struct list_t
+ * The singly linked list struct.
+ *
+ * @var list_t::head
+ *   @brief Pointer to the head ::node_t of the list.
+ *
+ * @var list_t::tail_sentinel
+ *   @brief Sentinel node. Its purpose is to determine the end of the list.
+ *
+ * @var list_t::number_of_elements
+ *   @brief Keeps track of the number of elements in the list at any given time.
+ */
+typedef struct {
     node_t* head;
     node_t* tail_sentinel;
-
     int number_of_elements;
 } list_t;
 
+/**
+ * @brief Initialize empty list.
+ *
+ * - Sets the head to NULL.
+ * - Sets the tail sentinel.
+ * - Sets the number of elements to zero.
+ *
+ * @param list_ptr Pointer to a ::list_t.
+ */
 void list_init(list_t* list_ptr);
+
+/**
+ * @brief Add an item to the end of the list.
+ *
+ * @param list_ptr Pointer to the ::list_t to be updated.
+ * @param val Value to be added to the list.
+ */
 void list_append(list_t* list_ptr, int val);
 void list_init_from_array(list_t* list_ptr, int *arr, size_t arr_size);
 void list_prepend(list_t* list_ptr, int val);
