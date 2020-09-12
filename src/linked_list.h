aba137fcf34a01d7ebf35257b7b1fd5fb297cc20
partmor <part.morales@gmail.com>
2020-09-12 16:45:37 +0200

    write minimal doc placeholder

--- src/linked_list.h
+++ src/linked_list.h
@@ -1,3 +1,9 @@
+/**
+ * @file linked_list.h
+ * @author Pedro Arturo Morales Maries
+ * @brief A simple implementation of a singly linked list of integers.
+ */
+
 #ifndef YACDSLIB_LINKED_LIST_H
 
 typedef struct node {
@@ -5,13 +11,34 @@ typedef struct node {
     struct node* next;
 } node_t;
 
-typedef struct list {
+/**
+ * @struct list_t
+ * The singly linked list struct.
+ *
+ * @var list_t::head
+ *   @brief Pointer to the head ::node_t of the list.
+ *
+ * @var list_t::tail_sentinel
+ *   @brief Sentinel node. Its purpose is to determine the end of the list.
+ *
+ * @var list_t::number_of_elements
+ *   @brief Keeps track of the number of elements in the list at any given time.
+ */
+typedef struct {
     node_t* head;
     node_t* tail_sentinel;
-
     int number_of_elements;
 } list_t;
 
+/**
+ * @brief Initialize empty list.
+ *
+ * - Sets the head to NULL.
+ * - Sets the tail sentinel.
+ * - Sets the number of elements to zero.
+ *
+ * @param list_ptr Pointer to a ::list_t.
+ */
 void list_init(list_t* list_ptr);
 void list_append(list_t* list_ptr, int val);
 void list_init_from_array(list_t* list_ptr, int *arr, size_t arr_size);
